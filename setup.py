6a1bfeb94c357b55ab243fb080371f36f747de39
Jason Furtney <jkfurtney@gmail.com>
2013-06-26 11:37:17 -0500

    Clean up for setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import os
 from setuptools import setup, find_packages
 
-def get_long_description():
-    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
-        return f.read()
+long_description = """
+Python connectivity for Itasca software.
+
+This library implements a connection via sockets between Python and
+the numerical modeling software from Itasca Consulting Group.
+
+www.itascacg.com/software
+
+FLAC, FLAC3D, PFC2D, PFC3D, UDEC & 3DEC
+
+See https://github.com/jkfurtney/itasca-python for more information.
+"""
 
 setup(
     name = 'itasca',
@@ -12,14 +21,19 @@ setup(
     author = 'Jason Furtney',
     author_email = 'jkfurtney@gmail.com',
     description = "Python conectivity for Itasca software",
-    long_description = get_long_description(),
+    long_description = long_description,
     keywords = 'Itasca FLAC FLAC3D PFC UDEC 3DEC',
+    license          = "BSD",
     tests_require = ['nose'],
-    install_requires = [],
+    install_requires = ['numpy >= 1.0.2'],
     packages = find_packages(),
     include_package_data = True,
     classifiers = [
         'Programming Language :: Python :: 2',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research"
     ],
     entry_points = { }
 )
