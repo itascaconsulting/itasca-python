ba6a299e8864c2d2cb6e4f6af829b8791206a832
gschwepp <g@schwepp.net>
2020-01-14 17:47:11 +0100

    fixing python syntax minor bugs in examples

--- tests/p2pClient_test.py
+++ tests/p2pClient_test.py
@@ -1,4 +1,4 @@
-from p2pLink import p2pLinkClient
+from itasca import p2pLinkClient
 import numpy as np
 
 with p2pLinkClient() as s:
@@ -10,7 +10,7 @@ with p2pLinkClient() as s:
     s.send_data(np.array([1,2,3]))
 
     adata = np.random.random((1000,1000))
-    print adata
+    print(adata)
     s.send_data(adata)
 
     for i in range(10):
--- tests/p2pServer_test.py
+++ tests/p2pServer_test.py
@@ -1,4 +1,6 @@
-from p2pLink import p2pLinkServer
+# p2pServer_test.py
+# Test server file to be run in Itasca software
+from itasca.util import p2pLinkServer
 import numpy as np
 
 with p2pLinkServer() as s:
@@ -7,8 +9,8 @@ with p2pLinkServer() as s:
     while True:
         a=s.read_data()
         if type(a) is int and a ==-1:
-            print "done"
+            print("done")
             break
-        print "got", a
+        print("got", a)
         if type(a) is np.ndarray:
-            print a.shape
+            print(a.shape)
